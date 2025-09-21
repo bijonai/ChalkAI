b12b30bb8023109f027eee85bb78c89d70bd6308
confuseder <g200803092018@163.com>
2025-09-21 10:07:17 +0800

    chore(eslint): Add eslint configs about typescript and vue.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,38 @@
 // @ts-check
-import withNuxt from './.nuxt/eslint.config.mjs'
+import { withNuxt } from "./.nuxt/eslint.config.mjs";
+import vueParser from "vue-eslint-parser";
+import tseslint from "typescript-eslint";
+import vue from "eslint-plugin-vue";
 
-export default withNuxt(
-  // Your custom configs here
-)
+export default withNuxt([
+  ...tseslint.configs.recommended,
+  ...vue.configs["flat/recommended"],
+  { ignores: ["**/node_modules/**", "**/dist/**"] },
+  {
+    files: ["{agent,app,packages,server,shared}**/*.{js,jsx,ts,tsx}"],
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: 2022,
+        sourceType: "module",
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    rules: {},
+  },
+  {
+    files: ["app/**/*.vue"],
+    languageOptions: {
+      parser: vueParser,
+      parserOptions: {
+        ecmaVersion: 2022,
+        sourceType: "module",
+        parser: {
+          js: "espree",
+          ts: tseslint.parser,
+        },
+      },
+    },
+    rules: {},
+  },
+]);
