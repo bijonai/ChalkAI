e864d36a7d080d2a1ccd6acede6ea5390f446213
Acbox <acbox0328@gmail.com>
2025-09-26 22:22:22 +0800

    fix(utils-theme): wrong css var name

--- packages/utils-theme/src/theme.ts
+++ packages/utils-theme/src/theme.ts
@@ -4,27 +4,32 @@ export function isCSSVariableExist(variableName: string): boolean {
   return value !== ''
 }
 
-export function size(name: string | number): string {
-  const variable = `--sci-size-${name}`
+type Size = 'primary' | '6xs' | '5xs' | '4xs' | '3xs' | '2xs' | 'xs' | 'sm' | 'md' | 'lg' | 'xl' | '2xl' | '3xl' | '4xl' | '5xl' | '6xl'
+export function size(name: Size |string | number): string {
+  const variable = `--chalk-size-${name}`
   return isCSSVariableExist(variable) ? `var(${variable})` : name.toString()
 }
 
-export function pallete(name: string): string {
-  const variable = `--sci-${name}`
+type Pallete = 'primary' | 'note' | 'warning' | 'accent' | 'alert' | 'info' | 'success' | 'creative'
+export function pallete(name: Pallete | string): string {
+  const variable = `--chalk-${name}`
   return isCSSVariableExist(variable) ? `var(${variable})` : name
 }
 
-export function highlight(name: string): string {
-  const variable = `--sci-highlight-${name}`
+type Highlight = 'primary' | 'key' | 'support' | 'creative' | 'caution' | 'info'
+export function highlight(name: Highlight | string): string {
+  const variable = `--chalk-highlight-${name}`
   return isCSSVariableExist(variable) ? `var(${variable})` : name
 }
 
-export function dasharray(name: string): string {
-  const variable = `--sci-dasharray-${name}`
+type Dasharray = 'solid' | 'dashed' | 'dotted'
+export function dasharray(name: Dasharray | string): string {
+  const variable = `--chalk-dasharray-${name}`
   return isCSSVariableExist(variable) ? `var(${variable})` : name
 }
 
-export function font(name: string): string {
-  const variable = `--sci-font-${name}`
+type Font = 'primary' | 'comic' | 'code' | 'math'
+export function font(name: Font | string): string {
+  const variable = `--chalk-font-${name}`
   return isCSSVariableExist(variable) ? `var(${variable})` : name
 }
