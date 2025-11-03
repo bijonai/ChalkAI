349bed81f150e3b251ffd119271279fce26b63e2
Acbox <acbox0328@gmail.com>
2025-11-03 21:28:20 +0800

    feat: rect align and baseline

--- packages/canvas/src/rect.ts
+++ packages/canvas/src/rect.ts
@@ -7,6 +7,8 @@ import { addPrefabKnowledge } from "@chalk-dsl/knowledge/default"
 export interface RectAttributes extends BaseCanvasElementAttributes, Fillable, Strokeable {
   width: number
   height: number
+  align: 'left' | 'center' | 'right'
+  baseline: 'top' | 'middle' | 'bottom'
 }
 
 export const rect = definePrefab<'rect', RectAttributes, { division: Vector2 }>((context) => {
@@ -16,10 +18,18 @@ export const rect = definePrefab<'rect', RectAttributes, { division: Vector2 }>(
       const root = createCanvasElementContainer(attrs, context.division)
       const [xDivision, yDivision] = context.division
 
+      const width = attrs.width * xDivision
+      const height = attrs.height * Math.abs(yDivision)
+      const offsetX = attrs.align === 'left' ? 0 : attrs.align === 'center' ? width / 2 : width
+      const offsetY = attrs.baseline === 'top' ? 0 : attrs.baseline === 'middle' ? height / 2 : height
       d3.select(root).append('path')
-        .attr('d', `M0,0 L${attrs.width * xDivision},0 L${attrs.width * xDivision},${attrs.height * Math.abs(yDivision)} L0,${attrs.height * Math.abs(yDivision)} Z`)
+        .attr('d', `M${offsetX},${offsetY} L${width + offsetX},${offsetY} L${width + offsetX},${height + offsetY} L${offsetX},${height + offsetY} Z`)
 
       return root
+    },
+    defaults: {
+      align: 'center',
+      baseline: 'middle'
     }
   }
 })
@@ -44,5 +54,15 @@ export const knowledge = definePrefabKnowledge<RectAttributes>((utils) => {
   utils.prop('height')
     .describe('The height of the rect')
     .type('number')
+
+  utils.prop('align')
+    .describe('The alignment of the rect, for example, "left" means the left edge of the rect is at the origin')
+    .type('"left" | "center" | "right"')
+    .optional('center')
+
+  utils.prop('baseline')
+    .describe('The baseline of the rect, for example, "top" means the top edge of the rect is at the origin')
+    .type('"top" | "middle" | "bottom"')
+    .optional('middle')
 })
 addPrefabKnowledge(knowledge)
